8907a5719dc56c152f3408ad08b13b14598f9aa5
Valeriia Zakharova <zakh.valeriia@gmail.com>
2020-11-19 18:34:24 +0200

    Alert has been added by tapping add

--- hitList(CoreData)/HitListViewController.swift
+++ hitList(CoreData)/HitListViewController.swift
@@ -22,6 +22,30 @@ class HitListViewController: UIViewController {
     }
 
     @IBAction func addName(_ sender: UIBarButtonItem) {
+        // creates an alert
+        let alert = UIAlertController(title: "New Note", message: "Add your new note here", preferredStyle: .alert)
+        
+        //creates save button, textField in which we can type our new note
+        let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+            guard let textField = alert.textFields?.first,
+                let notToSave = textField.text else { return }
+            
+            self.names.append(notToSave)
+            self.tableView.reloadData()
+        }
+        
+        // creates cancel button
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        // adds textField to the alert
+        alert.addTextField()
+        
+        //adds action buttons to the alert
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        //shows the alert
+        present(alert, animated: true)
     }
 }
 
@@ -33,6 +57,7 @@ extension HitListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = names[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
         return cell
     }
 }
