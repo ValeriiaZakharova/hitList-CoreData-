b10206e5ab96a78a010f1ee8c0a2850b1dd72d01
Valeriia Zakharova <zakh.valeriia@gmail.com>
2020-11-19 13:25:56 +0200

    TableView was added to the HitListViewController

--- hitList(CoreData)/ViewController.swift
+++ hitList(CoreData)/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HitListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var names: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "The List"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
-
+    @IBAction func addName(_ sender: UIBarButtonItem) {
+    }
 }
 
+extension HitListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+}
